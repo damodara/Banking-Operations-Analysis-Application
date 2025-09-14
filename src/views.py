e1bf0d914d9cd3af2dca26ece11407b72852d2ec
damodara <jay108@mail.ru>
2025-09-14 14:29:59 +0300

    upd func greetings with test

--- src/views.py
+++ src/views.py
@@ -2,19 +2,22 @@ from datetime import datetime
 def greetings(time_string):
     """
     Сообщение приветствия на основе полученного времени
-    :param dt: Дата в формате datetime, а именно YYYY-MM-DD HH:MM:SS
+    :param time_string: Дата в формате datetime, а именно YYYY-MM-DD HH:MM:SS
     :return: Строка с приветствием
     """
-    parsed_time = datetime.strptime(time_string, "%Y-%m-%d %H:%M:%S")
-    hour = parsed_time.hour
-    if 5 <= hour < 12:
-        return "Доброе утро"
-    elif 12 <= hour < 18:
-        return "Добрый день"
-    elif 18 <= hour < 23:
-        return "Добрый вечер"
-    else:
-        return "Доброй ночи"
+    try:
+        parsed_time = datetime.strptime(time_string, "%Y-%m-%d %H:%M:%S")
+        hour = parsed_time.hour
+        if 5 <= hour < 12:
+            return "Доброе утро"
+        elif 12 <= hour < 18:
+            return "Добрый день"
+        elif 18 <= hour < 23:
+            return "Добрый вечер"
+        else:
+            return "Доброй ночи"
+    except ValueError:
+        return "Неверный формат времени"
 
 
 if __name__ == "__main__": # pragma: no cover
--- tests/test_views.py
+++ tests/test_views.py
@@ -1,8 +1,30 @@
 from src.views import greetings
-from tests.conftest import valid_time_string
+import pytest
 
-def test_greetings(valid_time_string: str):
-    for input_date, expected_result in valid_time_string:
-        result = greetings(input_date)
-        assert result == expected_result
+
+@pytest.mark.parametrize(
+    "input_date, expected_result",
+    [
+        ("2025-09-14 04:10:05", "Доброй ночи"),
+        ("2025-09-14 05:10:05", "Доброе утро"),
+        ("2025-09-14 12:10:05", "Добрый день"),
+        ("2025-09-14 18:10:05", "Добрый вечер")
+    ]
+)
+def test_greetings_valid(input_date, expected_result):
+    assert greetings(input_date) == expected_result
+
+
+@pytest.mark.parametrize(
+    "invalid_input",
+    [
+        '',
+        'abc',
+        '2023-10-10',
+        '2023-10-10T10:30:00Z',
+        '2023-10-10 25:30:00',
+    ]
+)
+def test_greetings_invalid(invalid_input):
+    assert greetings(invalid_input) == "Неверный формат времени"
 
